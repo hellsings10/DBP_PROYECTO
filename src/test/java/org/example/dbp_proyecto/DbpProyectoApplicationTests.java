6111f172f8cc3c6c8bcfaf7cd2bf96bbf1deb496
hellsings10 <victor.soto@utec.edu.pe>
2026-05-20 11:25:39 -0500

    Configuracion basica del proyecto

---
+++ src/test/java/org/example/dbp_proyecto/DbpProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dbp_proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbpProyectoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
